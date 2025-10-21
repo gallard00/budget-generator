517205c53488ea2fcfb213955dc61424faf35f5d
Nahuel Gallardo <nahuel.ng240@gmail.com>
2025-10-21 00:24:08 -0300

    refactor: remove old backend structure and prepare for Angular integration

--- src/main/java/com/nahuelgallardo/budgetgenerator/repository/BudgeHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nahuelgallardo.budgetgenerator.repository;
-
-import com.nahuelgallardo.budgetgenerator.model.BudgetHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BudgeHistoryRepository extends JpaRepository<BudgetHistory, Long> {
-}
--- src/main/java/com/nahuelgallardo/budgetgenerator/service/IPdfService.java
+++
@@ -1,6 +0,0 @@
-package com.nahuelgallardo.budgetgenerator.service;
-import org.springframework.core.io.Resource;
-
-public interface IPdfService {
-    Resource exportBudgetToPdf(Long budgetId);
-}
